05e2bb167d1980187c3d9ddf6d27708378eaca1c
José Caldas <jrcaldas@yahoo.com.br>
2021-04-15 22:08:19 -0300

    install miragejs/axios/react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
+Modal.setAppElement("#root"); // acessibilidade
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTansactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTansactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
 
       <GlobalStyles />
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import image from "../../assets/logo.svg";
 import { Container, Content } from "./styled";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void; // não recebe nenhum parâmetro e não dá nenhum retorno
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={image} alt="logo" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
